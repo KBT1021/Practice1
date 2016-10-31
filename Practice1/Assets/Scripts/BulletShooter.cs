e97698bd1029227aa03f2f86248170333ad67643
KBT1021 <azabu19yuuki95@yahoo.co.jp>
2016-10-31 21:11:54 +0900

    [modify]弾を撃てるようにした（途中）

---
+++ Practice1/Assets/Scripts/BulletShooter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletShooter : MonoBehaviour {
+	public GameObject BulletPrefab;
+
+	public Transform Muzzle;
+
+	public float speed = 100;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Shoot ();
+		}
+	}
+	void Shoot(){
+		GameObject Bullets = Instantiate (BulletPrefab)as GameObject;
+		transform.position = Muzzle.position;
+		Vector3 force;
+		force = this.gameObject.transform.forward * speed;
+		Bullets.GetComponent<Rigidbody> ().AddForce (force);
+	}
+}
--- Practice1/Assets/Scripts/PlayerController.cs
+++ Practice1/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 10;
+	public GameObject Bullet;
 	void Start(){
 	}
 
 	void Update()
 	{
+		//位置の移動//
 		float x = Input.GetAxis ("Horizontal");
 		float z = Input.GetAxis ("Vertical");
 		transform.Translate (x * speed, 0, z * speed);
+		//弾の発射//
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			GameObject Bullets = Instantiate (Bullet)as GameObject;
+			Bullets.transform.position = transform.position;
+			Vector3 force;
+			force = this.gameObject.transform.forward * speed;
+			Bullets.GetComponent<Rigidbody> ().AddForce (force);
+		}
 	}
 }
