24bf9741588b58af63a05ac135656f30e85756ba
KBT1021 <azabu19yuuki95@yahoo.co.jp>
2016-11-02 18:49:43 +0900

    Revert "[modify]Enemyga"
    This reverts commit bc0684a61567875d15c0cd9fc568a35e406e14c4.

--- Practice1/Assets/Scripts/EnemyController.cs
+++ Practice1/Assets/Scripts/EnemyController.cs
@@ -2,16 +2,14 @@
 using System.Collections;
 
 public class EnemyController : MonoBehaviour {
-	//dz=dxの2次式の形で表す.このScriptにrotを組みこめば2次式の範囲でいろんな方向に動かせる//
-	public float x3co =0;
-	public float x2co = 10;
-	public float x1co = 0;
+	//dz=dxの2次式の形で表す.このScriptにrotを組みこめば3次式の範囲でいろんな方向に動かせる//
+	public float x2co = 0;
+	public float x1co = 10;
+	public float x0co = 0;
 	//Playerからの位置（スクロール対策）//
 	public float initialx = 10;
 	public float initialz = 10;
 	public float speed = 1;
-	public float speedx=1;
-	public float vanishtime=10.0f;
 	public GameObject Player;
 
 
@@ -20,13 +18,13 @@ public class EnemyController : MonoBehaviour {
 		transform.position=Player.transform.position;
 		transform.Translate (initialx, 0, initialz);
 	}
-		
+	
 	// Update is called once per frame
 	void Update () {
-		float delta = 0.1f;
+		float delta = 0.01f;
 		float dx = delta * speed;
-	//放物線上を動くようにしてみるz=x2co*dx^2+x1co*dx//
-		transform.Translate (dx*speedx,0,x3co*dx*dx*dx+x2co*dx*dx+x1co*dx);
-		Destroy (gameObject,vanishtime);
+	//放物線上を動くようにしてみるz=x2co*x^2+x1co*x+x0co//
+		transform.Translate(dx,0,x2co*dx*dx+x1co*dx+x0co);
+		Destroy (gameObject,10.0f);
 	}
 }
